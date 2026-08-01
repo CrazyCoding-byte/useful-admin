b09229eb0394032f91e185af9679c274d9006261
yzx18 <944166810@qq.com>
2026-08-02 00:42:58 +0800

    feat:添加优惠卷功能

--- model/src/main/java/com/yzx/model/product/vo/CategoryVo.java
+++ model/src/main/java/com/yzx/model/product/vo/CategoryVo.java
@@ -17,6 +17,10 @@ public class CategoryVo implements Serializable {
     private static final long serialVersionUID = 1L;
     private Long catId;
     private String name;
+    private Integer parentCid;
+    private Integer catLevel;
+    private Integer showStatus;
     private String icon;
+    private String productUnit;
     private List<CategoryVo> children;
 }
